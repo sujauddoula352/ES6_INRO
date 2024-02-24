2813debc2d80856bc33218747b7f6f4eda9222d6
Sujauddoula sohel <shsohel@Sujauddoulas-Mac-Pro.local>
2024-02-23 23:30:21 -0800

    chain es6 js

---
+++ chan.js
@@ -0,0 +1,18 @@
+const numbers =[4, 5, 7, 8, 9, 10];
+// function doubleIt(num){
+// console.log('num now', num);
+// return num * 2;
+// }
+// const result =numbers.map(doubleIt);
+// console.log(result);
+
+// const doubleto=(num) => num *2;
+ const result = numbers.map(num => num * 2 );
+ console.log(result);
+
+// for ( const num of numbers){
+//     const double = num * 2;
+//     doubled.push(double);
+
+// }
+// console.log(doubled);
